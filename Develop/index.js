528bab39142021f992580ab0be39f954aa9c135f
Tcomp <williamt7786@gmail.com>
2021-06-01 17:27:12 -0400

    this is for some of the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
-// TODO: Include packages needed for this application
 
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'Your name',
+      
+    },
+
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'Project name'
+    },
+    {
+        type: 'input',
+        message: 'What do you want in your table of contents?',
+        name: 'TOC'
+    },
+    {
+        type: 'input',
+        message: 'Describe your project!',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'What installation instructions do you have for users?',
+        name: 'Instructions'
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your Github?',
+        name: 'Github'
+    }
+  ])
+  
+  
+
+  
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
