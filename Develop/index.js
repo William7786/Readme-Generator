fd2097e9c1e0fd71cb14d91c2c1dbfb21848ba0c
Tcomp <williamt7786@gmail.com>
2021-06-02 15:36:51 -0400

    trying new functions

--- Develop/index.js
+++ Develop/index.js
@@ -2,14 +2,20 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require ("fs");
-const markdown = require ("./utils/generateMarkdown")
-const utils = require("util")
+// const markdown = require ("./utils/generateMarkdown")
+const utils = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 const writeFileSync = utils.promisify(fs.writeFile)
 
 // TODO: Create an array of questions for user input
-function questions () {
-    inquirer
-  .prompt([
+const questions = [
+
+      {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'Title'
+    },
+    
     {
       type: 'input',
       message: 'What is your name?',
@@ -17,11 +23,7 @@ function questions () {
       
     },
 
-    {
-        type: 'input',
-        message: 'What is the name of your project?',
-        name: 'Project name'
-    },
+  
     {
         type: 'input',
         message: 'What do you want in your table of contents?',
@@ -57,28 +59,40 @@ function questions () {
         message: 'What is the name of your Github?',
         name: 'Github'
     }
-  ])
-  
+
   
 
   
-};
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  
+fs.writeToFile(fileName, data , function(error) {
+  console.log(data)
+  console.log(fileName)
+  if(error){
+      return console.log(error)
+  } else {
+      console.log ("No errors here")
+  }
     
-}
+});
 
 // // TODO: Create a function to initialize app
-async function init() {
-try{
-const answers = await questions()
-const generate = markdown(answers)
-await writeFileSync ('./New/README.md', generate)
-}catch(error){
-console.log(error)
-}}
+function init() {
+inquirer.prompt(questions)
+ .then(function(data){
+     writeToFile("Readme.md", generateMarkdown(data));
+     console.log(data)
+ })
+
+
+// try{
+// const answers = await questions()
+// const generate = markdown(answers)
+// await writeFileSync ('./New/README.md', generate)
+// }catch(error){
+// console.log(error)
+}
 
 // Function call to initialize app
 init();
